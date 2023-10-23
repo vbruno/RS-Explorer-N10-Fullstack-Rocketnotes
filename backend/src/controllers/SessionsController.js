564c669e82fc5ce582409f9f4f5b9e09542df2da
Bruno S Velho <bruno.velho@gmail.com>
2023-10-23 10:37:06 -0300

    Aula: Gerando token do usuário

--- backend/src/controllers/SessionsController.js
+++ backend/src/controllers/SessionsController.js
@@ -1,24 +1,30 @@
 const knex = require('../database/knex')
 const AppError = require('../utils/AppError')
 const {compare} = require('bcryptjs')
+const authConfig = require('../configs/auth')
+const {sign} = require('jsonwebtoken')
 
 class SessionsController {
   async create(request, response) {
     const { email, password } = request.body;
 
     const user = await knex('users').where({email}).first();
-
     if (!user) {
       throw new AppError('Incorrect email/password combination', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
-
     if (!passwordMatched) {
       throw new AppError('Incorrect email/password combination', 401);
     }
 
-    return response.json({passwordMatched, user})
+    const {secret, expiresIn} = authConfig.jwt;
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn,
+    });
+
+    return response.json({user, token})
   }
 }
 
