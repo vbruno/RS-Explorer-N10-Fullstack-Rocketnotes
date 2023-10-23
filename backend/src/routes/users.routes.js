aea12a8bf6cdfcb4edbf1824b02014197ca43f20
Bruno S Velho <bruno.velho@gmail.com>
2023-10-23 16:34:22 -0300

    Aula: Salvando imagem no banco

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -3,12 +3,14 @@ const multer = require("multer");
 const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
 const ensureAuthenticated = require('../middleware/ensureAuthenticated')
 
 const usersRouter = Router();
 const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 usersRouter.post("/", usersController.create);
 usersRouter.put("/",ensureAuthenticated, usersController.update);
@@ -16,9 +18,7 @@ usersRouter.patch(
   "/avatar",
   ensureAuthenticated,
   upload.single("avatar"),
-  (request, response) => {
-    console.log(request.file.filename);
-    response.json({ ok: true });
-  })
+  userAvatarController.update
+)
 
 module.exports = usersRouter;
