6fc299a381d8abc499bf938042c64b5d8cc9f03d
Bruno S Velho <bruno.velho@gmail.com>
2023-10-25 11:05:03 -0300

    Aula: Adicionar Tags

--- frontend/src/pages/New/index.jsx
+++ frontend/src/pages/New/index.jsx
@@ -14,6 +14,9 @@ export function New() {
   const [links, setLinks] = useState([]);
   const [newLink, setNewLink] = useState("");
 
+  const [tags, setTags] = useState([]);
+  const [newTag, setNewTag] = useState("");
+
   function handleAddLink() {
     setLinks(prevState => [...prevState, newLink]);
     setNewLink("");
@@ -21,6 +24,11 @@ export function New() {
 
   function handleRemoveLink(deletedLink) {
     setLinks(prevState => prevState.filter(link => link !== deletedLink));
+  }
+
+  function handleAddTag() {
+    setTags(prevState => [...prevState, newTag]);
+    setNewTag("");
   }
 
   return (
@@ -53,8 +61,18 @@ export function New() {
 
           <Section title="Marcadores">
             <div className="tags">
-              <NoteItem value={"react"} />
-              <NoteItem isNew placeholder="Nova tag" />
+              {
+                tags.map((tag, index) => (
+                  <NoteItem key={index} value={tag} onClick={() => {}}/>
+                ))
+              }
+              <NoteItem
+                isNew
+                placeholder="Nova tag"
+                onChange={e => setNewTag(e.target.value)}
+                value={newTag}
+                onClick={handleAddTag}
+              />
             </div>
           </Section>
 
