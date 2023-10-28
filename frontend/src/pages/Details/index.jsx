1fc58303204ca551a62598712f3a79dcc3ee3949
Bruno S Velho <bruno.velho@gmail.com>
2023-10-28 18:35:50 -0300

    Aula: Carregando detalhes da nota

--- frontend/src/pages/Details/index.jsx
+++ frontend/src/pages/Details/index.jsx
@@ -1,50 +1,74 @@
-import { Header } from '../../components/Header'
-import { Button } from '../../components/Button'
-import { Section } from '../../components/Section'
-import { Tag } from '../../components/Tag'
-import { ButtonText } from '../../components/ButtonText'
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
-import { Container, Links, Content } from './styles'
+import { Container, Links, Content } from "./styles";
 
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
+import { ButtonText } from "../../components/ButtonText";
 
+import { api } from "../../services/api";
 
 export function Details() {
+  const [data, setData] = useState(null);
+
+  const params = useParams();
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate("/");
+  }
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`);
+      setData(response.data);
+    }
+
+    fetchNote();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Container>
       <Header />
 
-      <main>
-        <Content>
+      {data && (
+        <main>
+          <Content>
+            <ButtonText title={"Excluir nota"} />
 
-          <ButtonText title={'Excluir nota'} />
+            <h1>{data.title}</h1>
 
-          <h1>
-            Introdução ao React
-          </h1>
+            <p>{data.description}</p>
+            {data.links && (
+              <Section title={"Links úteis"}>
+                <Links>
+                  {data.links.map((link) => (
+                    <li key={String(link.id)}>
+                      <a href={link.url} target="_blank" rel="noreferrer">
+                        {link.url}
+                      </a>
+                    </li>
+                  ))}
+                </Links>
+              </Section>
+            )}
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque eos ipsum quaerat molestiae laboriosam ullam aliquid. Velit similique, fugit incidunt ad sint eveniet, earum aliquam ex voluptatem tempore cum veniam?
-          </p>
+            {data.tags && (
+              <Section title={"Marcadores"}>
+                {data.tags.map((tag) => (
+                  <Tag key={String(tag.id)} title={tag.name} />
+                ))}
+              </Section>
+            )}
 
-          <Section title={'Links úteis'}>
-            <Links>
-              <li>
-                <a href="#">https://www.rocketseat.com.br</a>
-              </li>
-              <li>
-                <a href="#">https://www.rocketseat.com.br</a>
-              </li>
-
-            </Links>
-          </Section>
-          <Section title={'Marcadores'}>
-            <Tag title={'express'} />
-            <Tag title={'node'} />
-          </Section>
-
-          <Button title={'Voltar'} />
-        </Content>
-      </main>
+            <Button title={"Voltar"} onClick={handleBack} />
+          </Content>
+        </main>
+      )}
     </Container>
-  )
+  );
 }
